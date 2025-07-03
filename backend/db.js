c143ce032bbef9a0e371cea5caee238e3bbc1982
vinodverma1399 <vinodverma2555@gmail.com>
2025-07-03 15:56:14 +0530

    database file add

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,14 @@
-// db.js
 
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ MongoDB connected successfully!');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully!');
   } catch (error) {
-    console.error('❌ MongoDB connection failed:', error.message);
-    process.exit(1); // agar DB connect nahi hua to server band kar do
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1); // exit app if DataBase not connected
   }
 };
 
