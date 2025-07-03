0a6111f6302461bb08eaa8fdd6096b3194a3f457
vinodverma1399 <vinodverma2555@gmail.com>
2025-07-03 15:57:54 +0530

    server file add and run port on 5000

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
-// server.js
-
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./db');  // yaha import kiya
+const express = require('express');// add express server file here 
+const cors = require('cors'); // require cops 
+require('dotenv').config(); // 
+const connectDB = require('./db');  //here we import database
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// MongoDB connect call karo
-  connectDB();
+// MongoDB connect and call
+  connectDB(); 
 
-// Middlewares
+// this is Middlewares 
 app.use(cors());
 app.use(express.json());
 
-// Test Route
+// this is test route
 app.get('/', (req, res) => {
-  res.send('🚀 ParkEase backend is running!');
+  res.send('ParkEase backend is running!');
 });
 
-// Start Server
+// Start Server on at 8000
 app.listen(PORT, () => {
-  console.log(`✅ Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
 
 
