03dec186fc0dd7012838dd50bc8048353f2d5546
vinodverma1399 <vinodverma2555@gmail.com>
2025-07-12 03:57:32 +0530

    Created Express app entry with middleware and routing

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./db');  // yaha import kiya
+const connectDB = require('./db');  
+const userRouter=require('./route/userRoute')
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+
+app.use('/api/user',userRouter)
+
+app.get('/',(req, res) => {
   res.send('🚀 ParkEase backend is running!');
 });
 
