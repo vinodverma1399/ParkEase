c709bc7a260634e43987aa696f836b6324de7cc1
vinodverma1399 <vinodverma2555@gmail.com>
2025-07-11 23:39:40 +0530

    Added .env and server.js database setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// server.js
-
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -7,20 +5,15 @@ const connectDB = require('./db');  // yaha import kiya
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+connectDB();
 
-// MongoDB connect call karo
-  connectDB();
-
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Test Route
 app.get('/', (req, res) => {
   res.send('🚀 ParkEase backend is running!');
 });
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`✅ Server started on port ${PORT}`);
 });
